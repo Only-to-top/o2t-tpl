3e4ca07a94989f1689a9101df1b79a38eefda2c3
Only to top <rlytrue@gmail.com>
2021-10-19 12:41:11 +0300

    Update 19.10.2021

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Fancybox
-    Fancybox.bind("[data-fancybox]", {
-        animationEffect: 'fade',
-        animationDuration: 555,
-        l10n: {
-            CLOSE: "Закрыть",
-            NEXT: "Далее",
-            PREV: "Назад",
-        },
-    });
-
     // Swiper
     if (document.querySelector(".swiper-1x")) {
         const swiper_1x = new Swiper('.swiper-1x', {
@@ -40,14 +29,56 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    // Fancybox
+    Fancybox.bind("[data-fancybox]", {
+        animationEffect: 'fade',
+        animationDuration: 555,
+        l10n: {
+            CLOSE: "Закрыть",
+            NEXT: "Далее",
+            PREV: "Назад",
+        },
+    });
+
+    // Forms
+    if (document.querySelector(".ajax-form")) {
+        const ajaxSend = async (url, formData) => {
+            const fetchResponse = await fetch(url, {
+                method: 'POST',
+                body: formData
+            });
+            if (!fetchResponse.ok) {
+                throw new Error(`Ошибка по адресу ${url}, статус ошибки ${fetchResponse.status}`);
+            }
+            return await fetchResponse.text();
+        }
+
+        document.querySelectorAll('.form').forEach(el => {
+            el.addEventListener('submit', function (e) {
+                e.preventDefault();
+                const formData = new FormData(this);
+
+                ajaxSend('/mail.php', formData)
+                    .then(function (data) {
+                        alert('Спасибо! Данные отправлены.');
+                        el.reset();
+                        // setTimeout(() => {
+                        //     document.querySelector('.fancybox-close-small').click(); // close fancy popup
+                        // }, 2000);
+                    }).catch(function (error) {
+                        alert(error);
+                    });
+            });
+        });
+    }
+
     // $("img, a").on("dragstart", e => e.preventDefault());
 });
 
 
 window.addEventListener('load', () => { });
 
+// + html { overflow-x: visible; }
+window.addEventListener('scroll', () => { });
 
 if (document.documentElement.clientWidth < 768) { };
-
-
-$(document).on('scroll touchmove', () => { });
